dd5fc54b218c23a42fd39f22789b0734e87a7047
Andrew Welker <awelker@pepperdash.com>
2020-02-20 11:31:08 -0700

    initial commit

---
+++ pepperdash-generic-colorlightZ6-epi/pepperdash-generic-colorlightZ6-epi/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+namespace pepperdash_generic_colorlightZ6_epi
+{
+    public class Class1
+    {
+    }
+}
+
