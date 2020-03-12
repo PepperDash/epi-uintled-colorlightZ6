8a5dc8c7daae9a3aae363aaca0c574f65933c4f5
Andrew Welker <andrew.welker@gmail.com>
2020-03-12 09:01:09 -0600

    added action and workflow

---
+++ .github/actions/pd-build/build.ps1
@@ -0,0 +1,37 @@
+Write-Output "Getting Latest Version"
+$latestVersions = $(git describe --tags $(git rev-list --tags='*.*.*' --max-count=10) --abbrev=0) 
+Write-Output "Latest Versions:" 
+Write-Output $latestVersions
+$latestVersion = ""
+Foreach ($version in $latestVersions) {
+  Write-Output $version
+  if ($version -match '^[1-9]+.\d+.\d+$') {
+    $latestVersion = $version
+    Write-Output "Setting latest version to: $latestVersion"
+    break
+  }
+}
+Write-Output "Latest: $latestVersion"
+Write-Output "Incrementing Version"
+$newVersion = [version]$latestVersion
+$phase = ""
+$newVersionString = ""
+switch -regex ($ENV:GITHUB_REF) {
+  '^origin\/master\/*.' {
+    $newVersionString = "{0}.{1}.{2}" -f $newVersion.Major, $newVersion.Minor, ($newVersion.Build + 1)
+  }
+  '^origin\/feature\/*.' {
+    $phase = 'alpha'
+  }
+  '^origin\/release\/*.' {
+    $phase = 'rc'
+  }
+  '^origin\/development\/*.' {
+    $phase = 'beta'
+  }
+  '^origin\/hotfix\/*.' {
+    $phase = 'hotfix'
+  }
+}
+$newVersionString = "{0}.{1}.{2}-{3}-{4}" -f $newVersion.Major, $newVersion.Minor, ($newVersion.Build + 1), $phase, $ENV:BUILD_NUMBER
+Write-Output "Incremented Version: $newVersionString"
