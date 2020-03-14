1bc27b6b384132b1fa631eae60fdede9ab4dae0f
Andrew Welker <andrew.welker@gmail.com>
2020-03-14 16:29:49 -0600

    fixed pipelining

--- .github/scripts/ZipBuildOutput.ps1
+++ .github/scripts/ZipBuildOutput.ps1
@@ -1,7 +1,6 @@
 New-Item -ItemType Directory -Force -Path "$($Env:GITHUB_WORKSPACE)\output"
 $exclusions = @(git submodule foreach --quiet 'echo $name')
-Get-ChildItem -recurse -Path "$Env:GITHUB_WORKSPACE" -include @("*.clz", "*.cpz", "*.cplz") |
-%{
+Get-ChildItem -recurse -Path "$($Env:GITHUB_WORKSPACE)" -include @("*.clz", "*.cpz", "*.cplz") | ForEach-Object{
   $allowed = $true;
   foreach($exclude in $exclusions) {
     if((Split-Path $_.FullName -Parent) -ilike $exclude) {
@@ -12,7 +11,7 @@ Get-ChildItem -recurse -Path "$Env:GITHUB_WORKSPACE" -include @("*.clz", "*.cpz"
   if($allowed) {
     $_;
   }
-} | Copy-Item -Destination $($Env:GITHUB_WORKSPACE)\output
-dir $($Env:GITHUB_WORKSPACE)\output
-Compress-Archive -Path "$($Env:GITHUB_WORKSPACE)\output\*" -DestinationPath "$($Env:GITHUB_WORKSPACE)\output.zip)"
-dir $($Env:GITHUB_WORKSPACE)
+} | Copy-Item -Destination "$($Env:GITHUB_WORKSPACE)\output"
+Get-ChildItem "$($Env:GITHUB_WORKSPACE)\output"
+Compress-Archive -Path "$($Env:GITHUB_WORKSPACE)\output\*" -DestinationPath "$($Env:GITHUB_WORKSPACE)\output.zip"
+Get-ChildItem "$($Env:GITHUB_WORKSPACE)\"
