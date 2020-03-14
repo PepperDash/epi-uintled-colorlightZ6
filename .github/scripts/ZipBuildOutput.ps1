6c25284bddfb4ce022a0e31ac498424f69198de0
Andrew Welker <andrew.welker@gmail.com>
2020-03-14 17:16:53 -0600

    changed Write-Output to Write-Host

--- .github/scripts/ZipBuildOutput.ps1
+++ .github/scripts/ZipBuildOutput.ps1
@@ -4,17 +4,17 @@ Get-ChildItem ($destination)
 $exclusions = @(git submodule foreach --quiet 'echo $name')
 Write-Output "Exclusions $($exclusions)"
 Get-ChildItem -recurse -Path "$($Env:GITHUB_WORKSPACE)" -include @("*.clz", "*.cpz", "*.cplz") | ForEach-Object{
-  Write-Output "checking $($_)"
+  Write-Host "checking $($_)"
   $allowed = $true;
   foreach($exclude in $exclusions) {
     if((Split-Path $_.FullName -Parent) -ilike $exclude) {
-      Write-Output "excluding $($_)"
+      Write-Host "excluding $($_)"
       $allowed = $false;
       break;
       }
     }
   if($allowed) {
-    Write-Output "allowing $($_)"
+    Write-Host "allowing $($_)"
     $_;
   }
 } | Copy-Item -Destination ($destination)
